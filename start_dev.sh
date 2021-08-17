c0671d619cb3db410c4bbf90e4177ea666a5a89b
Jakub Jakubowski <jakubjakubowski8@gmail.com>
2021-08-17 11:58:56 +0200

    Fix cert path

--- start_dev.sh
+++ start_dev.sh
@@ -2,7 +2,7 @@
 
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://34.207.216.185:2376"
-export DOCKER_CERT_PATH="${PWD}/../credentials/dev_env"
+export DOCKER_CERT_PATH="${PWD}/credentials/dev_env"
 
 /usr/bin/docker-compose -f docker-compose-non-dev.yml down
 /usr/bin/docker-compose -f docker-compose-non-dev.yml pull
